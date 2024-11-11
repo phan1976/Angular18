216457628b577ad438aa4548b585d4acb02fc349
vtp5335 <vtp5335@gmail.com>
2024-11-11 20:20:07 +0100

    ngfor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   public name:string = "Phan Van Thanh";
   public age = 16;
+  public traicay = ['tao','nho','cam'];
   public resetName(): void {
     // console.log("resetName");
     this.name = "";
     this.age = 0;
+  }
+
+  public ngOnInit():void {
+    console.log("trai cay = ",this.traicay);
   }
 
 }
