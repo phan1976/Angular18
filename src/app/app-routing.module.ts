56cf82317a45cb50ac6fa54d24dbf8688ee148af
vtp5335 <vtp5335@gmail.com>
2024-11-21 21:53:03 +0100

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataAppComponent } from './data-app/data-app.component';
 
-const routes: Routes = [];
+
+const routes: Routes [{path:'data', component: DataAppComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
-const routes: Routes[{path:'data', components: 'Data'}];
 
 export class AppRoutingModule {
   
