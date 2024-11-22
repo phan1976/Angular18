dbac9caf60fb230c40808b49fd9b81c6a82f76c6
vtp5335 <vtp5335@gmail.com>
2024-11-22 12:40:40 +0100

    up

--- src/app/Services/http-server.service.ts
+++ src/app/Services/http-server.service.ts
@@ -3,7 +3,27 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class HttpServerService {
+  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER_RANDOM_USERS = 'https://randomuser.me/api/?results=';
+  
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+      //authorization: 'my-auth-token'
+    })
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getComments(): Observable<any> {
+    const url = `${this.REST_API_SERVER}/commands`;
+    return this.httpClient.get<any>(url, this.httpOptions);
+  }
+}
+  
+  export class HttpServerService {
   private REST_API_CONTENTS = 'http://localhost:3000';
   private httpOptions = {
     headers: new HttpHeaders({
