e25f8e478fe6dd502afc8e95c4df009804747de4
vtp5335 <vtp5335@gmail.com>
2024-11-24 10:46:11 +0100

    up

---
+++ src/app/Services/http-server.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServerService {
+  private REST_API_COMMENTS = 'http://localhost:3000';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getComment(): Observable<any> {
+    const url = `${this.REST_API_COMMENTS}/comments`;
+    return this.httpClient.get<any>(url, this.httpOptions);
+  }
+}
---
+++ src/app/data-app/data-app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HttpServerService } from '../Services/http-server.service';
+
+@Component({
+  selector: 'app-data-app',
+  templateUrl: './data-app.component.html',
+  styleUrls: ['./data-app.component.scss']
+})
+export class DataAppComponent {
+  constructor(private httpServer: HttpServerService){}
+  public ngOnInit(){
+    this.httpServer.getComment().subscribe(data => {
+      console.log('comments = ', data)
+    });
+  }
+
+}
