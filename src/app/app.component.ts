c3c3a55a27c35fd96ae858575d9dadbb464cab43
vtp5335 <vtp5335@gmail.com>
2024-11-26 22:15:41 +0100

    qrcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular18';
+  public title = 'QR';
+  public qrInfo = '';
+  public width = 300;
 }
