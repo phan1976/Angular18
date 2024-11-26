ccf3f8a177f018fd412adfb289b551f1d77fc184
vtp5335 <vtp5335@gmail.com>
2024-11-26 15:46:22 +0100

    cha

---
+++ src/app/cha/cha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cha',
+  templateUrl: './cha.component.html',
+  styleUrls: ['./cha.component.scss']
+})
+export class ChaComponent {
+
+}
